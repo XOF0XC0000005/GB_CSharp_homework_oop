095ee692bc716e5ebc73474b73f55c3c2ed3513b
XOF0XC0000005 <leva95reva@yahoo.com>
2024-04-12 10:46:33 +0300

    Update AdultFamilyMember.cs

--- HomeWork3/AdultFamilyMember.cs
+++ HomeWork3/AdultFamilyMember.cs
@@ -2,7 +2,7 @@ namespace HomeWork3
 {
     internal class AdultFamilyMember : FamilyMember
     {
-        public AdultFamilyMember(string name, int age, Gender.Sex sex, AdultFamilyMember father, AdultFamilyMember mother, AdultFamilyMember spouse, List<FamilyMember> childrens) 
+        public AdultFamilyMember(string name, int age, Gender.Sex sex, AdultFamilyMember father, AdultFamilyMember mother, AdultFamilyMember spouse, List<FamilyMember> childrens)
         {
             Name = name;
             Age = age;
